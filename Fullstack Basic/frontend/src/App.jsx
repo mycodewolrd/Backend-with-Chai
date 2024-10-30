bd9e2d6439869980c52133b86e0c7d7ca9899472
Rajnandini <rajnandinipatra2003@gmail.com>
2024-10-30 21:22:50 +0530

    2nd commit

---
+++ Fullstack Basic/frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })
+
+  return (
+    <>
+      <h1> Frontend Ready</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {
+        jokes.map((joke, i) => {
+          return (
+            <div key={joke.id}>
+              <h3>{joke.title}</h3>
+              <p>{joke}</p>
+            </div>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default App;
